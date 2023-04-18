14eb49bebd4090f1b5c380687dbe8b27c36e18c9
영준 배 <byj6614@naver.com>
2023-04-18 20:20:44 +0900

    feat : ListClass 인스턴트
    인스턴스를 생성시 만들어지는 배열

--- 0418/ListClass/ListClass.cs
+++ 0418/ListClass/ListClass.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace _0418.ListClass
 {
-    internal class ListClass
+    internal class ListClass<T>
     {
+        private const int arnum = 10;
+        private T[] array;
+        private int size;
+
+        public ListClass()
+        {
+            this.array = new T[arnum];              // 인스턴스를 생성 할때 만들어지는 배열
+            this.size = 0;                          // 인스턴스를 생성 하면서 생기는 사이즈
+        }
+        
     }
 }
