cf5a9718397199581c5b6c952211098ed79f2ee9
영준 배 <byj6614@naver.com>
2023-04-19 21:36:00 +0900

    feat : 기술면접 조사

---
+++ 0419/LinkedList/number2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0419.LinkedList
+{
+    internal class number2
+    {
+        /*
+         * LinkedList : 각 노드가 데이터와 포인터를 가지고 한 줄로 연결되어있는 방식의 자료구조
+         *              노드의 포인터가 이전 노드와 다음 노드와의 연결을 담당
+         *              Node를 제거하거나 추가할시 앞 뒤에 Node만 바뀌며 그 외의 것은 변함이 없다.
+         *              추가 하거나 제거 할시 인덱스가 밀리거나 땡겨지지 않음
+         *              인덱스가 없어 탐색할때 순차적으로 해야할 필요가 있음
+         */
+    }
+}
