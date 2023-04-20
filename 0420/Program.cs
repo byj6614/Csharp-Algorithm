01583855b13ff091e1e4fd117fdf1f352ab3ffe8
영준 배 <byj6614@naver.com>
2023-04-20 20:30:47 +0900

    feat : LinkedList foreach 실행

--- 0420/Program.cs
+++ 0420/Program.cs
@@ -9,11 +9,17 @@
         static void Main(string[] args)
         {
             ABC.List<int> list=new ABC.List<int>();
+            ABC.LinkedList<int> linkedList=new ABC.LinkedList<int>();
             for(int i=1;i<=5;i++)
             {
                 list.Add(i);
+                linkedList.AddLast(i);
             }
             foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            foreach(int i in linkedList)
             {
                 Console.WriteLine(i);
             }
