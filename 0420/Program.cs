3b46b695ba77a302b7fcd6d6bab3fddde1f142b0
영준 배 <byj6614@naver.com>
2023-04-20 19:42:24 +0900

    feat: 0420과제

---
+++ 0420/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _0420
+{
+    internal class Program
+    {
+        /*
+         * 1.List,LinkedList IEnumberable 구현
+         * 2.foreach에 List,LinkedList 반복 확인
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
