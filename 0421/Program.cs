ad50951b2e0324262622d5d0f82d3c6dc98143dd
영준 배 <byj6614@naver.com>
2023-04-21 21:27:59 +0900

    feat : 괄호 계산기

--- 0421/Program.cs
+++ 0421/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace _0421
+﻿using _0421.addition.Stack;
+
+namespace _0421
 {
     internal class Program
     {
+        static void Test1()     //괄호 계산기
+        {
+            Stack<char> stack = new Stack<char>();
+            bracket bracket = new bracket();
+            stack.Push('(');
+            stack.Push(')');
+            stack.Push('(');
+            stack.Push('(');
+            stack.Push(')');
+            stack.Push(')');
+            bracket.Breacket(stack);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Test1();
         }
     }
 }
---
+++ 0421/addition/Stack/bracket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0421.addition.Stack
+{
+    internal class bracket
+    {
+        
+        public void Breacket(Stack<char> chars)
+        {
+            char[] abc= chars.ToArray();
+            int a=0;
+            for(int i=0;i<abc.Length;i++)
+            {
+                if (abc[i] == '(')
+                    a++;
+                else if (abc[i] == ')')
+                    a--;
+            }
+            if(a==0)
+            {
+                Console.WriteLine("정답");
+            }
+            else
+            {
+                Console.WriteLine("오답");
+            }
+        }
+    }
+}
