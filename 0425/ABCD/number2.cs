f572e82dc6f89f60ee7a7742963f4036928f86b0
영준 배 <byj6614@naver.com>
2023-04-25 21:13:41 +0900

    feat : 이진탐색트리의 한계점과 극복방법

---
+++ 0425/ABCD/number2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0425.ABCD
+{
+    internal class number2
+    {
+        /*
+         * 이진탐색트리의 한계점과 극복방법
+         * 이진 탐색 트리의 한계점은 한쪽 자식으로만 추가되면 발생하는 불규칙적인 이진형 트리이다 
+         * 이경우 시간복잡도가 O(log n)에서 O(n)으로 증가하게 된다.
+         * 극복 방법은 자가균형기능을 추가한 트리를 사용하며 불균형이 일어날 때 회전하여 불균형이 있는 상황을 해결한다. 
+         * 자가균형기능을 사용하는 트리(Red-Black Tree,AVL Tree)를 사용하여 대체한다.
+         */
+    }
+}
