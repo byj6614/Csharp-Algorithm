3f1b57720de74faf16db34239b778b9dd82bb7ba
영준 배 <byj6614@naver.com>
2023-04-26 20:49:07 +0900

    feat : 해싱과 해시함수에 대한 조사

---
+++ 0426/NewFolder/number2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0426.NewFolder
+{
+    internal class number2
+    {
+        /*
+         * 해싱과 해시함수에 대한 조사
+         * 해시 함수는 임의의 길이를 갖는 데이터를 고정된 길이의 데이터로 변환시켜주는 함수이며
+         * 해시함수의 결과가 밀집도가 낮아야 한다 밀집도가 높아지면 해시 함수는 느려지면서 의미가 없어진다.
+         * 그래서 해시테이블의 크기가 클수록 효율이 좋아지며 커진 만큼 밀집도가 낮아지는 확률이 줄어들며 밀집도가 커지면서 해시 함수가 빨라진다.
+         * 해싱은 키 값을 받을때 그 값을 인덱스로 바꾸는 과정을 해싱이라고 한다.
+         */
+    }
+}
