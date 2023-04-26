fff3bc3f032074fcfb801d156a989d984400a814
영준 배 <byj6614@naver.com>
2023-04-26 20:50:00 +0900

    feat : 해시테이블의 충돌과 충돌 해결방안

---
+++ 0426/NewFolder/number3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0426.NewFolder
+{
+    internal class number3
+    {
+        /*
+         * 해시테이블의 충돌과 충돌해결 방안
+         * 
+         * 해시테이블에 지정할 경우 key값을 해싱하는 과정에서 key의 값이 같아지는 경우가 있다. 이때 충돌이 발생하는데
+         * 이 때의 해결 방안으로 체이싱과 개방주소법이 있는데 체이싱은 충돌이 발생 할 때 LinkedList를 통해 연결하는 방식이며
+         * 개방주소법은 충돌이 발생하면 다른 빈 공간에 데이터를 삽입하는것이다
+         * c#은 Node를 쓰는걸 선호하지 않으므로 주로 개방주소법을 통해 해결한다.
+         */
+    }
+}
