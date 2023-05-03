781bef0e6deac95f4b3bb28c0fb45f168d62278e
영준 배 <byj6614@naver.com>
2023-05-03 21:49:31 +0900

    feat : PathFinding

---
+++ PathFinding/Tilemap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinding
+{
+    internal class Tilemap
+    {
+        /******************************************************
+		 * 타일맵 (Tilemap)
+		 * 
+		 * 2차원 평면의 그래프를 정점이 아닌 위치를 통해 표현하는 그래프
+		 * 위치의 이동가능 유무만을 가지는 타일맵과,
+		 * 타일의 종류를 표현한 이차원 타일맵이 있음
+		 * 인접한 이동가능한 위치간 간선이 있으며 가중치는 동일함
+		 ******************************************************/
+
+        //타일맵은 bool을 통해 이미 지나갔거나 안지나간지 확인해준다
+
+        // <타일맵 그래프>
+        // 예시 - 위치의 이동가능 표현한 이차원 타일맵
+        bool[,] tileMap1 = new bool[7, 7]
+        {
+            { false, false, false, false, false, false, false },
+            { false,  true,  true, false, false, false, false },
+            { false, false,  true, false, false,  true, false },
+            { false, false,  true,  true,  true,  true, false },
+            { false, false,  true, false, false, false, false },
+            { false, false,  true,  true,  true,  true, false },
+            { false, false, false, false, false, false, false },
+        };
+        /*
+		 * ■ ■ ■ ■ ■ ■ ■
+		 * ■   ■   ■ ■ ■
+		 * ■   ■   ■   ■
+		 * ■   ■       ■
+		 * ■   ■   ■ ■ ■
+		 * ■           ■
+		 * ■ ■ ■ ■ ■ ■ ■
+		 */
+    }
+
+}
